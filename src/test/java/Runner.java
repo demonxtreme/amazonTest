6b4819fd4d9be106e1ea6b6c544298230bf88935
Alan <alan.epanno@gmail.com>
2017-06-15 23:27:11 +0100

    cucumber reports

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,6 +6,6 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
  * Created by alanpanno on 28/05/2017.
  */
 
-@CucumberOptions(features = {"src/test/features"} , glue = "steps")
+@CucumberOptions(features = {"src/test/features"} , glue = "steps", format = {"pretty", "html:target/Destination"})
 public class Runner extends AbstractTestNGCucumberTests {
 }
